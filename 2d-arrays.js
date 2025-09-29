da44b1151f5df66947426afe19a986058f9f9f42
Alex Fore <forealexander4@gmail.com>
2025-09-29 11:04:51 -0500

    built program with three 2d arrays: allScores, theaterSeating, contactsList.

--- 2d-arrays.js
+++ 2d-arrays.js
@@ -1,1 +1,22 @@
 
+// Challenge 1
+let allScores = [
+    [54, 78, 34],
+    [99, 100, 98],
+    [23, 56, 89]
+];
+
+// Challenge 2
+let theaterSeating = [
+    ["A1", "A2", "A3", "A4"],
+    ["B1", "B2", "B3", "B4"],
+    ["C1", "C2", "C3", "C4"]
+];
+
+// Challenge 3
+let contactsList = [
+    ["Larry", "larry@gmail.com", 2174566622],
+    ["Breanda", "brenda.portrafka@gmail.com", 7468883746]
+];
+
+// console.log(`${allScores} ${theaterSeating} ${contactsList}`);
